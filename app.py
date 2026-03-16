76192e2e7db32be5dcb8a59aa9a0b27f8058aea8
yaxuanyang94-sudo <yaxuanyang94@gmail.com>
2026-03-17 01:14:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from datetime import datetime
 # 1. 之前那個 CSV 下載網址
 CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSum58yVAkFdh7baIrQ0PnrYys2FnSp4xGbG-oiAILDztPH6MDEtc4-q2fiijT3hO4PfvhUOjcaHkF5/pub?output=csv" 
 # 2. 剛剛拿到的 Apps Script 網址
-GAS_URL = "https://script.google.com/macros/s/AKfycbwxxuK_AHNNVU6_TbTXDGwvv44P7LrHkNpQjbxqxzRpOoEGwW4u0m9sbWwvllvH7Vc6/exec"
+GAS_URL = "https://script.google.com/macros/s/AKfycbwVpp2g4m-s3hnMtFm5Khr_iM7QXxqlSQykIYBEkh9r5-2wzu_69I8Zah9gXKWAV4pa/exec"
 
 st.set_page_config(page_title="列印金庫-終極版", layout="centered")
 st.title("🖨️ 列印金庫 (一鍵儲值)")
